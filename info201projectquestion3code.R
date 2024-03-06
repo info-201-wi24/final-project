a761403bebe0fc205dbbac53dd4cdec1fcddc837
badnickton <155212455+badnickton@users.noreply.github.com>
2024-03-05 16:59:31 -0800

    Update info201projectquestion3code.R

--- info201projectquestion3code.R
+++ info201projectquestion3code.R
@@ -10,10 +10,9 @@ aggregated_data <- merged_data %>%
   group_by(Year) %>%
   summarise(Total_Acres_Burned = sum(AcresBurned, na.rm = TRUE))
 
-# Line graph showing total acres burned for each year with a trend line
-ggplot(aggregated_data, aes(x = Year, y = Total_Acres_Burned)) +
-  geom_point() +
-  geom_smooth(method = "lm", se = FALSE, color = "red") +  # Add trend line
+# Bar chart for each year showing total acres burned
+ggplot(aggregated_data, aes(x = as.factor(Year), y = Total_Acres_Burned)) +
+  geom_bar(stat = "identity", fill = "skyblue") +
   labs(title = "Total Acres Burned by Year",
        x = "Year",
        y = "Total Acres Burned")
